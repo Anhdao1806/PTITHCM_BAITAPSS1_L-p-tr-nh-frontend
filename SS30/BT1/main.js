cbe02588466090922606290e0a438ee8d6184c33
Anhdao1806 <daoleho123@gmail.com>
2025-04-08 22:30:07 +0700

    Add files via upload

---
+++ SS30/BT1/main.js
@@ -0,0 +1,111 @@
+let products = [
+    ["mì tôm", 5, 5000],
+    ["bánh mì", 12, 15000],
+    ["bánh bao", 5, 8000],
+    ["mèn mén", 30, 20000],
+  ];
+  let cart = [];
+  
+  let choice;
+  do {
+    choice = Number(
+      prompt("Mời nhập lựa chọn:\n1. Xem danh sách sản phẩm\n2. Chọn sản phẩm\n3. In hóa đơn\n4. Sắp xếp các vật phẩm theo giá\n5. Thoát chương trình"));
+    switch (choice) {
+      case 1:
+        showProduct();
+        break;
+      case 2:
+        addToCart();
+        break;
+      case 3:
+        printBill();
+        break;
+      case 4:
+        let sortChoice = Number(prompt("1. Tăng dần\n2. Giảm dần\n3. Thoát."));
+        switch(sortChoice){
+            case 1:
+                sortAscending();
+                break;
+            case 2: 
+                sortDescending();
+                break;
+            case 3:
+                console.log("Thoát.");
+                break;
+            default:
+                console.log("Lựa chọn không hợp lệ.");                    
+        }
+        if (sortChoice === 4) {
+            break;
+        }
+        break;
+      case 5: 
+        console.log("Thoát chương trình.");
+        break;
+      default:
+        console.log("Lựa chọn không hợp lệ.");
+        break;
+    }
+    if(choice === 5){
+        break;
+    }
+  } while (choice !== 5);
+  
+  function showProduct() {
+    console.log("Danh sách sản phẩm:");
+    for (let i = 0; i < products.length; i++) {
+      console.log(
+        `${i + 1}. ${products[i][0]} - Số lượng: ${products[i][1]} - Giá: ${products[i][2]}đ`);
+    }
+  }
+  function addToCart() {
+    let productName = prompt("Mời bạn nhập sản phẩm cần mua");
+    let flag = -1;
+    for (let i = 0; i < products.length; i++) {
+      if (products[i][0] === productName) {
+        flag = i;
+        break;
+      }
+    }
+    if (flag === -1) {
+      console.log("Sản phẩm ko có trong cửa hàng");
+    } else {
+      console.log("Sản phẩm bạn cần mua có trong cửa hàng");
+      if (products[flag][1] === 0) {
+        console.log("Sản phẩm đã hết hàng");
+      } else {
+        console.log("Sản phầm còn hàng");
+        products[flag][1]--;
+        let index = -1;
+        for (let i = 0; i < cart.length; i++) {
+          if (cart[i][0] === productName) {
+            index = i;
+            break;
+          }
+        }
+        if (index === -1) {
+          //sản phẩm chưa có trong giỏ hàng
+          cart.push([productName, 1, products[flag][2]]);
+        } else {
+          cart[index][1]++;
+        }
+      }
+    }
+  }
+  function printBill() {
+    console.log(cart);
+    console.log(products);
+    let sum = 0;
+    for (let i = 0; i < cart.length; i++) {
+      sum += cart[i][1] * cart[i][2];
+    }
+    console.log(` Tổng số tiền phải trả: ${sum}`);
+  }
+  function sortAscending(){
+    products.sort((a, b) => a[2] - b[2]);
+    console.log("Danh sách sản phẩm được sắp xếp theo giá tăng dần");
+  }
+function sortDescending(){
+    products.sort((a, b) => b[2] - a[2]);
+    console.log("Danh sách sản phẩm được sắp xếp theo giá giảm dần");
+  }
